760535b3846417ca1ac65303825a10c43832da44
Amr <legithamouda@gmail.com>
2025-03-27 06:10:11 +0200

    Updates the data accessed that's used in weather card

--- src/locationData.js
+++ src/locationData.js
@@ -1,19 +1,29 @@
-export { locationData };
-const weatherApiKey = "8QNJGXAPYFNUSS3G9WT6862CE";
+export { getLocationData };
+import { format } from "date-fns";
 
-function locationData(locationName) {
-    fetch(
-        `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/` +
-        locationName +
-        `?unitGroup=metric&include=days&key=8QNJGXAPYFNUSS3G9WT6862CE&contentType=json
+async function getLocationData(locationName) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/` +
+      locationName +
+      `?unitGroup=metric&include=days&key=8QNJGXAPYFNUSS3G9WT6862CE&contentType=json
 `,
-        {
-            method: "GET",
-            header: {},
-            mode: "cors",
-        },
-    )
-        .then((response) => response.json())
-        .then((response) => console.log(response))
-        .catch((err) => console.error(err));
+    { mode: "cors" },
+  );
+  const jsonData = await response.json();
+  const address = await jsonData.resolvedAddress;
+  const celsius = await jsonData.days[0].temp;
+  const condition = await jsonData.days[0].conditions;
+  const dateTime = await jsonData.days[0].datetime;
+
+  //Dom
+  const cityName = document.querySelector(".location-name");
+  const temperature = document.querySelector(".temperature");
+  const description = document.querySelector(".description");
+  const date = document.querySelector(".date");
+
+  cityName.textContent = address;
+  temperature.textContent = celsius + "°C";
+  description.textContent = condition;
+  date.textContent = format(dateTime, "PPPP");
+  console.log(jsonData.days[0]);
 }
